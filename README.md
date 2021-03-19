96eb795d3c51cd044576b0f35b2629caecb5e949
jarred <jarred.caris@gmail.com>
2021-03-19 17:17:29 +1030

    final push with read me completed and video attached

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # e-commerce_back_end
 Object-Relational Mapping (ORM): E-Commerce Back End
+
+
+
+![ecommerce](Assets/ecommerce.PNG)
+
+  ## Description
+  Create an ecommerce backend system for manipulating databases. This is for users who want to view, update and delete their products in their online stores. 
+
+  * [Installation](#installation)
+  * [Technology](#technology)
+  * [Success](#success)
+  * [Holdbacks](#holdbacks)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [License](#license)
+  * [Questions](#questions)
+  
+
+## Installation
+npm run seed and npm start in terminal. Also need to install express, sequalize and MYSQL2. Lastly the database will need to be created in mysql workbench
+
+## Technology
+Express.js, sequalize, MYSQL2, MYSQL workbench, POSTMAN
+
+## Success
+I had a great time with this project. It looked alot harder than it ended up being which was a nice suprise. I got my models done very quickly and then started on the routes. I got through the get, get all and delete routes fairly easily. The others took some time but in the end, once one route folder was completed, it was pretty much copy and paste into the others and making the relevant adjustments. 
+
+## Holdbacks
+Code wise, I didnt struggle as much on this one as I have with previous ones. It was alot of new concepts but i found the class assignments and working with class mates made it quite a simple process. My major issues came with using postman to be honest. IVe only had a little exposure to it and practicing with it was a little more time consuming then i wanted. So it took me longer to test my code. 
+
+
+## Contribution
+We were given alot of code to start off with. We had to create the models, the routes and make sure all the relevant add ons were installed. With a little troubleshooting with the tutor and solving a problem or 2 with my study group, I was able to write the majority of the code by myself with some small tweaks from outside help and research. 
+
+## Test
+no testing 
+
+## License ![license badge](https://img.shields.io/badge/License-MIT-<COLOR>)
+
+
+
+
+## Questions
+If you have fursther questions, you can reach me at
+[Github](https://github.com/Jarred-Caris)
+or
+jarred.caris@gmail.com
+
+[click for video] (https://drive.google.com/file/d/1FGKL9BO5TSSME4vQabOq0SZzxckqyJW5/view)
+
+
+[click for URL] (https://jarred-caris.github.io/note_taker/)
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,58 +7,58 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const categoryAll = await Category.findAll({
+    const allTags = await Tag.findAll({
       include: [{ model: Product }],
     });
-    res.status(200).json(categoryAll);
+    res.status(200).json(allTags);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const categorySingle = await Category.findByPk(req.params.id, {
+    const singleTag = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
-    if (!categorySingle) {
+    if (!singleTag) {
       res.status(404).json({ message: "No id found!" });
       return;
     }
 
-    res.status(200).json(categorySingle);
+    res.status(200).json(singleTag);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const categoryNew = await Category.create({
+    const tagNew = await Tag.create({
       id: req.body.id,
-      category_name: req.body.category_name,
+      tag_name: req.body.tag_name,
     });
-    res.status(200).json(categoryNew);
+    res.status(200).json(tagNew);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const categoryUpdate = await Category.update(req.body, {
+    const tagUpdate = await Tag.update(req.body, {
       // category_name: req.body.category_name,
       where: {
         id: req.params.id,
       },
     });
-    res.status(200).json(categoryUpdate);
-    if (!categoryUpdate) {
+    res.status(200).json(tagUpdate);
+    if (!tagUpdate) {
       res.status(404).json({ message: `Can not locate ID` });
     }
   } catch (err) {
@@ -66,22 +66,21 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async  (req, res) => {
   // delete on tag by its `id` value
   try {
-    const categoryDelete = await Category.destroy({
-      //  category_name: req.body.category_name,
+    const tagDelete = await Tag.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (!categoryDelete) {
-      res.status(404).json({ message: "No category found with this id!" });
+    if (!tagDelete) {
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
 
-    res.status(200).json(categoryDelete);
+    res.status(200).json(tagDelete);
   } catch (err) {
     res.status(500).json(err);
   }
